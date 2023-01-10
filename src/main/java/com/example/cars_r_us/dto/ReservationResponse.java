fece4c5affa980c028dc9f42dab9844af5634ebb
DoroHD <david.bjakobsen@gmail.com>
2023-01-10 23:49:34 +0100

    Fix builder bug in reservation response

--- src/main/java/com/example/cars_r_us/dto/ReservationResponse.java
+++ src/main/java/com/example/cars_r_us/dto/ReservationResponse.java
@@ -11,7 +11,6 @@ import javax.persistence.ManyToOne;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Builder
 public class ReservationResponse {
 
     public ReservationResponse(Reservation reservation) {
